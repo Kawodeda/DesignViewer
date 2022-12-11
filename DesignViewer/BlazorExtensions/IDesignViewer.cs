e08a8d949b649525810a1d1ca072516352c431d4
Daniil Chuprikov <d.chuprikov@aurigma.com>
2022-12-11 17:11:54 +0700

    add SelectedElementFillColor property to DV

--- DesignViewer/BlazorExtensions/IDesignViewer.cs
+++ DesignViewer/BlazorExtensions/IDesignViewer.cs
@@ -1,4 +1,5 @@
 ﻿using Model.Design;
+using Model.Design.Appearance.Color;
 using Model.Design.Math;
 
 namespace BlazorExtensions
@@ -9,12 +10,12 @@ namespace BlazorExtensions
 
         public Element? SelectedElement { get; set; }
 
+        public Color? SelectedElementFillColor { get; set; }
+
         public Affine2DMatrix Transform { get; }
 
         public Design Design { get; set; }
 
         public int CurrentSurfaceIndex { get; set; }
-
-        public void ChangeSelectedElementColor(string value);
     }
 }
