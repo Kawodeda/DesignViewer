dd7f49ca9dd3c70f4a55ac760eaeebebdf07d315
Daniil Chuprikov <d.chuprikov@aurigma.com>
2022-12-13 17:39:45 +0700

    add client AssetService

---
+++ DesignViewer/BlazorExtensions/Services/AssetService.cs
@@ -0,0 +1,56 @@
+﻿using ApiClient;
+using BlazorExtensions.Services.Exceptions;
+using System.Net;
+
+namespace BlazorExtensions.Services
+{
+    public class AssetService : IAssetService
+    {
+        private readonly IAssetsApiClient _assetsApiClient;
+        private readonly Dictionary<string, Stream> _assets = new Dictionary<string, Stream>();
+
+        public AssetService(IAssetsApiClient assetsApiClient)
+        {
+            _assetsApiClient = assetsApiClient;
+        }
+
+        public async Task<Stream> GetAssetAsync(string storageId)
+        {
+            if (!_assets.ContainsKey(storageId))
+            {
+                await RequestAssetAsync(storageId);
+            }
+
+            return CopyData(_assets[storageId]);
+        }
+
+        public async Task RequestAssetAsync(string storageId)
+        {
+            try
+            {
+                using FileResponse response = await _assetsApiClient.GetAssetContnetAsync(storageId);
+
+                _assets.Add(storageId, CopyData(response.Stream));
+            }
+            catch (ApiException exception)
+            {
+                if (exception.StatusCode == (int)HttpStatusCode.NotFound)
+                {
+                    throw new AssetNotFoundException(storageId, $"Could not found requested asset with storageId: {storageId}");
+                }
+
+                throw new AssetLoadingException(storageId, $"Could not load requested asset with storageId: {storageId}");
+            }
+        }
+
+        private Stream CopyData(Stream source)
+        {
+            var result = new MemoryStream();
+            source.Position = 0;
+            source.CopyTo(result);
+            result.Position = 0;
+
+            return result;
+        }
+    }
+}
---
+++ DesignViewer/BlazorExtensions/Services/Exceptions/AssetNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorExtensions.Services.Exceptions
+{
+    public class AssetNotFoundException : AssetLoadingException
+    {
+        public AssetNotFoundException(string? message = null, string? storageId = null)
+            : base(message, storageId) { }
+    }
+}
